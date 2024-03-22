564c4c0c9c3880a939e696f9079170898ea164bd
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2024-03-22 08:03:29 -0500

    DashBoard y sección categoría

---
+++ src/app/backoffice/category/category.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'backoffice-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.css'
+})
+export class CategoryComponent {
+
+  title = 'Categoría';
+}
---
+++ src/app/backoffice/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'backoffice-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+
+}
